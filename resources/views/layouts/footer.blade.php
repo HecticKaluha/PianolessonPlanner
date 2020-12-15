7a3c6dc2542f960dcba26f410af330286b588cc8
Stefano <sverhoeve@davinci.nl>
2020-12-15 23:25:31 +0100

    added master layout setup
    also added homecontroller and fixed the flow to be inline with the framework

---
+++ resources/views/layouts/footer.blade.php
@@ -0,0 +1,7 @@
+<footer>
+    <ul>
+        <li><a href="#">Location</a></li>
+        <li><a href="#">Contact</a></li>
+        <li><a href="#">Social media</a></li>
+    </ul>
+</footer>
---
+++ resources/views/layouts/master.blade.php
@@ -0,0 +1,57 @@
+<!DOCTYPE html>
+<!--[if IE 9]><html class="ie9 no-focus" lang="en"><![endif]-->
+<!--[if gt IE 9]><!--><html class="no-focus" lang="en"><!--<![endif]-->
+<head>
+    <meta charset="utf-8">
+
+    <title>Pianoplanning</title>
+
+    <meta name="description" content="Website for planning">
+    <meta name="author" content="Stefano Verhoeve">
+    <meta name="robots" content="noindex, nofollow">
+    <meta name="viewport" content="width=device-width,initial-scale=1.0,user-scalable=0">
+
+    <link rel="apple-touch-icon" sizes="180x180" href="{{asset('img/favicons/.png')}}">
+    <link rel="icon" type="image/png" sizes="32x32" href="{{asset('img/favicons/.png')}}">
+    <link rel="icon" type="image/png" sizes="16x16" href="{{asset('img/favicons/.png')}}">
+
+    @stack('style')
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+    <link href="{{ asset('css/style.css') }}" rel="stylesheet">
+
+    <script src="https://code.jquery.com/jquery-3.5.1.min.js" integrity="sha256-9/aliU8dGd2tb6OSsuzixeV4y/faTqgFtohetphbbj0=" crossorigin="anonymous"></script>
+
+</head>
+<body>
+<div id="page-loader"></div>
+
+{{--@include('layouts.aside')--}}
+
+@include('layouts.nav')
+
+@if($flash = session('message'))
+    <div id="flash-message" class="alert alert-success" role="alert">{{$flash}}</div>
+@endif
+@if($errors->has('error'))
+    <div id="flash-message" class="alert alert-danger" role="alert">{{$errors->first('error')}}</div>
+@endif
+
+{{--@include('layouts.header')--}}
+
+    <main id="main-container">
+        @yield('content')
+    </main>
+
+@include('layouts.footer')
+
+<script src="{{asset('js/script.js')}}"></script>
+
+@stack('scripts')
+
+
+<script>
+    $('div.alert').not('.alert-important').delay(3000).fadeOut(350);
+</script>
+
+</body>
+</html>
