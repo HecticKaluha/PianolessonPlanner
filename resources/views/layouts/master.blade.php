19b470f7391c4e10ba6e98e2315bb4c1a4964c64
Stefano <sverhoeve@davinci.nl>
2020-12-16 23:26:37 +0100

    added free theme by Creative Tim
    theme used:
    Tailwind starter kit by Creative Tim
    https://www.creative-tim.com/learning-lab/tailwind-starter-kit/presentation

--- resources/views/layouts/master.blade.php
+++ resources/views/layouts/master.blade.php
@@ -16,8 +16,11 @@
     <link rel="icon" type="image/png" sizes="16x16" href="{{asset('img/favicons/.png')}}">
 
     @stack('style')
-    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+{{--    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">--}}
+
+    <script src="https://kit.fontawesome.com/c33e61f09c.js" crossorigin="anonymous"></script>
     <link href="{{ asset('css/style.css') }}" rel="stylesheet">
+    <link href="{{ asset('css/app.css') }}" rel="stylesheet">
 
     <script src="https://code.jquery.com/jquery-3.5.1.min.js" integrity="sha256-9/aliU8dGd2tb6OSsuzixeV4y/faTqgFtohetphbbj0=" crossorigin="anonymous"></script>
 
@@ -38,9 +41,9 @@
 
 {{--@include('layouts.header')--}}
 
-    <main id="main-container">
-        @yield('content')
-    </main>
+
+@yield('content')
+
 
 @include('layouts.footer')
 
