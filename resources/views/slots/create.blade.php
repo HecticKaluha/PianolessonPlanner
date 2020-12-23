071229d17f398c68159956b10c2c6c56725f355b
Stefano <sverhoeve@davinci.nl>
2020-12-23 21:25:44 +0100

    added a datepicker

--- resources/views/slots/create.blade.php
+++ resources/views/slots/create.blade.php
@@ -1,4 +1,4 @@
-@push('style')
+@push('styles')
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/bootstrap-datepicker/1.9.0/css/bootstrap-datepicker.min.css"/>
 @endpush
 
@@ -14,7 +14,7 @@
             <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
                 <div class="p-6 bg-white border-b border-gray-200">
 
-                    <form class=""
+                    <form class="bootstrap"
                           action="/slot/store"
                           method="post">
                         @csrf
@@ -25,33 +25,43 @@
 
 {{--                        select days--}}
 
-{{--                        select time--}}
+{{--                        select starttime--}}
+
+{{--                        select endtime--}}
 
 
-                        <div class="bootstrap-iso bootstrap">
-                            <div class="container-fluid">
-                                <div class="row">
-                                    <div class="col-md-6 col-sm-6 col-xs-12">
+                        <div class="form-group">
+                            <label class="control-label" for="startDate">Start date</label>
+                            <input class="form-control" data-datepicker id="startDate" name="startDate" placeholder="MM/DD/YYY" type="text"/>
+                        </div>
 
-                                        <!-- Form code begins -->
-                                        <form method="post">
-                                            <div class="form-group"> <!-- Date input -->
-                                                <label class="control-label" for="date">Date</label>
-                                                <input class="form-control" id="date" name="date" placeholder="MM/DD/YYY" type="text"/>
-                                            </div>
-                                            <div class="form-group"> <!-- Submit button -->
-                                                <button class="btn btn-primary " name="submit" type="submit">Submit</button>
-                                            </div>
-                                        </form>
-                                        <!-- Form code ends -->
+                        <div class="form-group">
+                            <label class="control-label" for="endDate">End date</label>
+                            <input class="form-control" data-datepicker id="endDate" name="endDate" placeholder="MM/DD/YYY" type="text"/>
+                        </div>
 
-                                    </div>
-                                </div>
+                        <div class="form-group">
+                            <label class="" for="days">Select days</label>
+                            <div>
+                                <input type="checkbox" name="days" value="1"> Monday
+                                <input type="checkbox" name="days" value="2"> Tuesday
+                                <input type="checkbox" name="days" value="3"> Wednesday
+                                <input type="checkbox" name="days" value="4"> Thursday
+                                <input type="checkbox" name="days" value="5"> Friday
+                                <input type="checkbox" name="days" value="6"> Saturday
+                                <input type="checkbox" name="days" value="0"> Sunday
                             </div>
                         </div>
 
+                        <div class="form-group">
+                            <label class="control-label" for="startTime">Start time</label>
+                            <input class="form-control" data-timePicker id="startTime" name="startTime" placeholder="HH:MM" type="text"/>
+                        </div>
 
-                        <label for="startTime">Start time</label>
+                        <div class="form-group">
+                            <label class="control-label" for="endTime">End time</label>
+                            <input class="form-control" data-timePicker id="endTime" name="endTime" placeholder="HH:MM" type="text"/>
+                        </div>
 
                     </form>
 
@@ -64,8 +74,8 @@
         <script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/bootstrap-datepicker/1.9.0/js/bootstrap-datepicker.min.js"></script>
         <script>
             $(document).ready(function(){
-                var date_input=$('input[name="date"]'); //our date input has the name "date"
-                var container=$('.bootstrap-iso form').length>0 ? $('.bootstrap-iso form').parent() : "body";
+                var date_input=$('input[data-datepicker]');
+                var container=$('.bootstrap form').length>0 ? $('.bootstrap form').parent() : "body";
                 var options={
                     format: 'mm/dd/yyyy',
                     container: container,
@@ -73,7 +83,11 @@
                     autoclose: true,
                 };
                 date_input.datepicker(options);
+
+                $('#startDate').datepicker();
+                $('#endTime').datepicker();
             })
+
         </script>
     @endpush
 </x-app-layout>
