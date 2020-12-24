42d766e5ed0374129ccc571deeaa165b9c78c939
Stefano <sverhoeve@davinci.nl>
2020-12-24 02:31:43 +0100

    updaded dependencies for datatable.buttons

--- resources/views/planning.blade.php
+++ resources/views/planning.blade.php
@@ -1,11 +1,11 @@
 @push('styles')
     <link rel="stylesheet" href="https://cdn.datatables.net/1.10.23/css/dataTables.bootstrap5.min.css">
-    <link rel="stylesheet" href="https://cdn.datatables.net/buttons/1.0.3/css/buttons.dataTables.min.css">
+    <link rel="stylesheet" href="https://cdn.datatables.net/buttons/1.6.4/css/buttons.dataTables.min.css">
 @endpush
 @push('scripts')
     <script src="https://cdn.datatables.net/1.10.23/js/jquery.dataTables.min.js"></script>
     <script src="https://cdn.datatables.net/1.10.23/js/dataTables.bootstrap5.min.js"></script>
-    <script src="https://cdn.datatables.net/buttons/1.0.3/js/dataTables.buttons.min.js"></script>
+    <script src="https://cdn.datatables.net/buttons/1.6.4/js/dataTables.buttons.min.js"></script>
     <script src="{{asset('vendor/datatables/buttons.server-side.js')}}"></script>
 @endpush
 <x-app-layout>
